b255b23ea3cef506d6d85ad3ec5e6cd0f0d91506
Claudine Chionh <github@claudinec.net>
2021-08-22 17:51:12 +1000

    update Things creation scripts

--- cc-things-create-todo-single.js
+++ cc-things-create-todo-single.js
@@ -1,10 +1,14 @@
 // Create a single todo item in Things.
 // Uses draft title, body, tags.
+// Tags must already be known to Things.
 
 require('cc-things-functions.js');
 
 var todo = TJSTodo.create();
-todo.title = draft.title;    // string
-todo.notes = draft.content;  // string
-todo.tags = draft.tags;     // array of strings
-thingsContainer([todo]);
+todo.title = draft.title;
+todo.notes = draft.content;
+for (n in draft.tags) {
+    todo.addTag(draft.tags[n]);
+}
+message = thingsContainer([todo]);
+alert(message);
--- cc-things-functions.js
+++ cc-things-functions.js
@@ -1,18 +1,21 @@
 // Functions for Things integration.
 
 function thingsContainer(thingArray) {
-        // Example from https://scripting.getdrafts.com/classes/tjscontainer
+    // Example from https://scripting.getdrafts.com/classes/tjscontainer
 
-        // Create a container to handle creation of Things URL
-        var container = TJSContainer.create(thingArray);
+    // Create a container to handle creation of Things URL
+    var container = TJSContainer.create(thingArray);
 
-        // Use CallbackURL object to open URL in Things
-        var cb = CallbackURL.create();
-        cb.baseURL = container.url;
-        var success = cb.open();
-        if (success) {
-                console.log("Created in Things");
-        } else {
-                context.fail();
-        }
+    // Use CallbackURL object to open URL in Things
+    var cb = CallbackURL.create();
+    cb.baseURL = container.url;
+    var success = cb.open();
+    var message = '';
+    if (success) {
+        message = 'Successfully created in Things';
+    } else {
+        message = 'Failed';
+        context.fail();
+    }
+    return message;
 }
