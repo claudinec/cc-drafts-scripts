72de0d2592290924bacb1d28e397dedadfba24ce
Claudine Chionh <github@claudinec.net>
2021-08-24 17:07:23 +1000

    fix error in updating metadata

--- cc-text-revau-lookup.js
+++ cc-text-revau-lookup.js
@@ -1,7 +1,7 @@
 /**
  * @file Drafts script that searches Reverse Australia for the given phone number.
- * @author 0.1.0 Chionh <info@claudinec.net>
- * @version 0.0.1
+ * @author Claudine Chionh <info@claudinec.net>
+ * @version 0.1.0
  */
 const regex = /\+|\s/g;
 const revauQuery = 'https://www.reverseaustralia.com/lookup/'
