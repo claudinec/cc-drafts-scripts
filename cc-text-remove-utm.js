6effd07b401d81fb5bc3ad303078c012e73c03bf
Claudine Chionh <info@claudinec.net>
2021-09-15 11:39:31 +1000

    Fix global regexes, use replaceAll

--- cc-text-remove-utm.js
+++ cc-text-remove-utm.js
@@ -1,5 +1,5 @@
 // Remove Google tracking from URL.
-const regex = /(http.*)(\?|\&)(utm_[a-z]+=.*)/g
+const regex = /(http.*)(\?|\&utm_[a-z]+=.*)+?/g
 const textOld = draft.content
-const textNew = textOld.replace(regex, "$1")
+const textNew = textOld.replaceAll(regex, "$1")
 draft.content = textNew
